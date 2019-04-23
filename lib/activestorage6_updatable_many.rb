c9628b453982d1725c54ff154ae0c10fe25058bc
ichi <ichi.ttht.1@gmail.com>
2019-04-23 15:03:50 +0900

    bundle gem

---
+++ lib/activestorage6_updatable_many.rb
@@ -0,0 +1,6 @@
+require "activestorage6_updatable_many/version"
+
+module Activestorage6UpdatableMany
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/activestorage6_updatable_many/version.rb
@@ -0,0 +1,3 @@
+module Activestorage6UpdatableMany
+  VERSION = "0.1.0"
+end
